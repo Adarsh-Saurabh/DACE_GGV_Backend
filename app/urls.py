1f0c146d817c50a7dc837713577b1ec028ad4548
Adarsh Saurabh <75805225+Adarsh-Saurabh@users.noreply.github.com>
2022-06-22 22:43:27 +0530

    tried to correct advertisement

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,13 @@
 from . import views
 from django.urls import path
+from django.contrib import admin
 
+# Djnago admin header customization
+admin.site.site_header = 'Dace admin'
+admin.site.site_title = 'Dace admin'
+admin.site.index_title = 'Welcome to Dace admin panel!'
 
+  
 urlpatterns = [
     path('', views.home, name='home'),
     path('syllabus/', views.syllabus, name='syllabus'),
