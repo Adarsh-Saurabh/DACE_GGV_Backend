4e2d3c3b51aa09a4558eba58df3e159c9e70956b
Adarsh Saurabh <75805225+Adarsh-Saurabh@users.noreply.github.com>
2022-07-09 13:03:14 +0530

    admin changes

--- app/admin.py
+++ app/admin.py
@@ -6,7 +6,7 @@ class LoginAdmin(admin.ModelAdmin):
     list_display = [ 'name', 'registration_no', 'profile']
     # filters out the attributes with which we can perform search_fields
     list_filter = ['name', 'registration_no']
-    # Creates a search box in the admin page
+    # Creates a search box in the admin page    
     search_fields = ['name', 'registration_no']
 
 # Register your models here.
